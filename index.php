f4d592ed10242240f9fef46a6f288efe7b6318d4
natmacira <natimciraolo@gmail.com>
2023-02-08 16:01:38 -0300

    html first content

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>EnamorESI</title>
+	<link rel="stylesheet" href="style.min.css">
+	<script src="js/main.js"></script>
+	<link rel="stylesheet" href="https://use.typekit.net/tgm5fbl.css">
+	<!-- <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="img/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png"> -->
+	<!-- <link rel="manifest" href="img/favicon/manifest.json"> -->
+	<!-- <meta name="msapplication-TileColor" content="#0066ff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#0066ff">
+	<meta itemProp="image" property="og:image" content="https://nodo.org.ar/img/favicon/android-icon-192x192.png" />
+	<meta property="og:description" content="Combatimos la inseguridad alimentaria impactando positivamente en el medio ambiente.">
+	<meta property="og:locale" content="es_LA">
+	<meta property="og:image" content="https://nodo.org.ar/img/favicon/android-icon-192x192.png" />
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://nodo.org.ar/" /> -->
+</head>
+
+<body>
+	<header>
+		<section class="principal-logos">
+			<picture>
+				<!-- Desktop -->
+				<source media="(min-width:980px)" srcset="img/Logo-ESI@2x.png" alt="Logo Esi Igualdad">
+				<!-- Mobile  -->
+				<img src="img/Logo-ESI.png" alt="Logo Esi Igualdad">
+			</picture>
+			<picture>
+				<!-- Desktop -->
+				<source media="(min-width:980px)" srcset="img/Logo-Amnesty@2x.png" alt="Logo Amnistía Internacional">
+				<!-- Mobile  -->
+				<img src="img/Logo-Amnesty.png" alt="Logo Amnistía Internacional">
+			</picture>
+		</section>
+		<picture class="enamoresi-title">
+			<!-- Desktop -->
+			<source media="(min-width:980px)" srcset="img/enamoresi-@2x.png" alt="EnamorEsi">
+			<!-- Mobile  -->
+			<img src="img/enamoresi-.png" alt="EnamorEsi">
+		</picture>
+		<section class="subtitle">
+			<p>ESTE 14 DE FEBRERO, <strong>HAZ MATCH </strong>CON TUS DERECHOS.</p>
+		</section>
+	</header>
+
+	<main>
+		<section class="slider-card">
+
+		</section>
+		<section class="text-main">
+			<p>COMPARTE TU MATCH CON EL DERECHO QUE MÁS VAYA CONTIGO Y ESTE 8M SAL A LAS CALLES A RECLAMARLO.</p>
+			<p>POR UNA EDUCACIÓN SEXUAL INTEGRAL PARA TODES.</p>
+		</section>
+	</main>
+
+	<footer>
+		<p>SÍGUENOS EN</p>
+		<ul>
+			<li> <a href="facebook.com/WikimediaArgentina"><img src="img/facebook@2x.png" alt="Link a Facebook"> </a></li>
+			<li> <a href="facebook.com/WikimediaArgentina"><img src="img/twitter@2x.png" alt="Link a twitter"> </a></li>
+			<li> <a href="instagram.com/wikimedia_ar"><img src="img/youtubex2.png" alt="Link a youtube"> </a></li>
+			<li> <a href="twitter.com/wikimedia_ar"><img src="img/instagram@2x.png" alt="Link a instagram"> </a></li>
+		</ul>
+	</footer>
+</body>
