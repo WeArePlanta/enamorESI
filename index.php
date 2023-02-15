ef00b7344fce98c5c727f201b3fe1f74bdd1b25b
natmacira <natimciraolo@gmail.com>
2023-02-15 10:08:39 -0300

    re upload fonts

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 	<script src="js/main.js"></script>
 	<link rel="stylesheet" href="swiper-bundle.min.css" />
 	<script src="js/swiper-bundle.min.js"></script>
+	<link rel="stylesheet" href="https://use.typekit.net/tgm5fbl.css">
 	<link rel="apple-touch-icon" sizes="57x57" href="img/favicon-esi/apple-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="img/favicon-esi/apple-icon-60x60.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon-esi/apple-icon-72x72.png">
